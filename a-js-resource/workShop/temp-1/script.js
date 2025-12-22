f525bb6b55b8dd137442bed8d3347ba7dd6ce402
Michael Appiah <appiahm29@yahoo.com>
2025-12-22 03:03:12 +0000

    all

---
+++ a-js-resource/workShop/temp-1/script.js
@@ -0,0 +1,11 @@
+ const box = document.getElementById('colorBox');
+        const button = document.getElementById('colorButton');
+        
+        // When button is clicked
+        button.onclick = function() {
+            // Make a random color
+            const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+            
+            // Change box color
+            box.style.backgroundColor = randomColor;
+        };
